13de0fac114399af82a4c2d6636815a9da4b583a
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2022-05-23 12:55:12 +0200

    Correct router

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
 
   router: {
-    base: "humour-news-light"
+    base: "/humour-news-light/"
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
