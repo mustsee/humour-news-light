91a5858ee54712517f6d21b88f9f49a81b156b37
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2022-06-04 17:33:08 +0200

    Add gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/fireauth.js'],
+  plugins: ['~/plugins/fireauth.js', { src: '~/plugins/gtag.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
